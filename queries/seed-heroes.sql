720e7438d4f4fe3c8d21f49d17258bb2f488147f
Mickael - desktop <knopmickael@gmail.com>
2025-05-13 00:00:17 -0300

    heroes seeder update

--- queries/seed-heroes.sql
+++ queries/seed-heroes.sql
@@ -1,4 +1,6 @@
 INSERT INTO heroes (name, hp, atk, def, profile_picture) VALUES
-  ('Slime de Fogo', 30, 7, 3, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQQRTPqSnbsSQp7wYw3g4yBJAY_-sGhcqsfeolgkfNN0Hrc?width=128'),
-  ('Slime de Gelo', 45, 2, 10, 'foobar'),
-  ('Slime de Vento', 35, 4, 4, 'foobar')
+  ('Red Slime', 35, 12, 6, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQR-5b5C2qjfQaB1ySwZKBqRAWT4zvmhOdG_8OJJ0LS9kIs?width=128&height=128'),
+  ('Blue Slime', 50, 7, 3, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQQvIJi3NdG2TpHa9KQRppfNAQDLLol3Tm4AWBv0mg-wF9U?width=128&height=128'),
+  ('Green Slime', 70, 3, 5, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQQUT2-9WG1WSK-5eaaY4ypeAZeC08UJcmpuh2oHkPOgpoI?width=128&height=128'),
+  ('Purple Slime', 40, 8, 8, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQRZoZpuOpDXT5ycuwjAZuHVAQagVbVWJd2k4J0ClZM0vCk?width=128&height=128'),
+  ('Yellow Slime', 55, 7, 5, 'https://1drv.ms/i/c/4ddf50075e4db0e6/IQS_wOhVG5OfR4YwxsqnhAL-AWvSXZkx4ui01_fOAQKNMGc?width=128&height=128')
