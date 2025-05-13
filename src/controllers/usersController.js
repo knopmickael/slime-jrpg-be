e6849734544b737e6684b4cfd2481fd0468cec34
Mickael - desktop <knopmickael@gmail.com>
2025-05-13 00:31:50 -0300

    criando set-lph endpoint

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -28,7 +28,9 @@ const getUserByUsername = (req, res) => {
   const { username } = req.params;
 
   try {
-    const user = db.prepare("SELECT * FROM users WHERE username = ?").get(username);
+    const user = db
+      .prepare("SELECT * FROM users WHERE username = ?")
+      .get(username);
 
     if (!user) {
       return res.status(404).send("Usuário não encontrado");
@@ -40,4 +42,25 @@ const getUserByUsername = (req, res) => {
   }
 };
 
-module.exports = { listUsers, getCurrentUser, getUserByUsername };
+const setLastPickedHero = (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const result = db.prepare("UPDATE users SET last_picked_hero = ?").run(id);
+
+    if (result.changes === 0) {
+      return res.status(404).send("Usuário não encontrado");
+    }
+
+    res.status(200).send("Último herói escolhido atualizado com sucesso");
+  } catch (e) {
+    res.status(500).send("Erro ao atualizar o último herói escolhido");
+  }
+};
+
+module.exports = {
+  listUsers,
+  getCurrentUser,
+  getUserByUsername,
+  setLastPickedHero,
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 
-router.get("/list", usersController.listUsers);
 router.get("/me", usersController.getCurrentUser);
+router.get("/list", usersController.listUsers);
+router.post("/set-lph", usersController.setLastPickedHero);
 router.get("/:username", usersController.getUserByUsername);
 
 module.exports = router;
